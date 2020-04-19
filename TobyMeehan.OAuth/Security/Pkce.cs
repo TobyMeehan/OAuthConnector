9ce88487c4a0d461ae1fbc576fadd2b28def294e
Toby <toby@tobymeehan.com>
2020-04-19 18:46:29 +0100

    Add PKCE
    Added Pkce class for generating PKCE verifiers and challenges

---
+++ TobyMeehan.OAuth/Security/Pkce.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TobyMeehan.OAuth.Security
+{
+    class Pkce
+    {
+        public static string GenerateVerifier()
+        {
+            RandomNumberGenerator rng = new RNGCryptoServiceProvider();
+            Random random = new Random();
+
+            byte[] buffer = new byte[random.Next(43, 129)];
+            rng.GetBytes(buffer);
+
+            return Convert.ToBase64String(buffer);
+        }
+
+        public static string ChallengeFromVerifier(string codeVerifier)
+        {
+            string challenge = Convert.ToBase64String(new SHA256Managed().ComputeHash(Encoding.UTF8.GetBytes(codeVerifier)));
+
+            challenge = (challenge.EndsWith("=") ? challenge.Remove(challenge.LastIndexOf("=")) : challenge).Replace("+", "-").Replace("/", "_");
+
+            return challenge;
+        }
+    }
+}

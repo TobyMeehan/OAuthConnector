db127144de67e67e6686cd8c9c154600afd1e287
Toby <toby@tobymeehan.com>
2020-06-02 17:28:46 +0100

    Add custom collections
    Added custom collections for handling entities

---
+++ TobyMeehan.OAuth/Collections/IEntityCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TobyMeehan.OAuth.Models;
+
+namespace TobyMeehan.OAuth.Collections
+{
+    public interface IEntityCollection<T> : IEnumerable<T> where T : IEntity
+    {
+        T this[string id] { get; }
+    }
+}
---
+++ TobyMeehan.OAuth/Collections/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TobyMeehan.OAuth.Models;
+
+namespace TobyMeehan.OAuth.Collections
+{
+    public interface IRepository<T> : IEntityCollection<Footprint<T>>
+    {
+        Task<T> GetAsync(string id);
+        Task<T> GetAsync(Footprint<T> footprint);
+
+        Task<T> CreateAsync(object value);
+    }
+}

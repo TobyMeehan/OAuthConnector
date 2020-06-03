583cfd6c8b32c25b67bb479637f8599bfee21415
Toby <toby@tobymeehan.com>
2020-06-03 17:16:15 +0100

    Add OAuthClient options
    Added options configuration class for use with DI providers

---
+++ TobyMeehan.OAuth/ApiVersion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TobyMeehan.OAuth
+{
+    public class ApiVersion
+    {
+        public static ApiVersion Version2
+        {
+            get
+            {
+                string url = "https://api.tobymeehan.com/api";
+                string authoriseUrl = "https://tobymeehan.com/oauth";
+
+                return new ApiVersion
+                {
+                    Url = url,
+                    AuthoriseUrl = authoriseUrl,
+                    Endpoints =
+                    {
+                        {"authorize", $"{authoriseUrl}/authorize" },
+                        {"token", $"{authoriseUrl}/token" },
+                        {"account", $"{url}/account" },
+                        {"application", $"{url}/application" },
+                        {"download", $"{url}/download" },
+                        {"scoreboard", $"{url}/scoreboard" },
+                        {"transaction", $"{url}/transaction" }
+                    }
+                };
+            }
+        }
+
+
+        public string Url { get; set; }
+        public string AuthoriseUrl { get; set; }
+        public Dictionary<string, string> Endpoints { get; set; }
+
+    }
+}
---
+++ TobyMeehan.OAuth/OAuthClientOptions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TobyMeehan.OAuth
+{
+    public class OAuthClientOptions
+    {
+        public ApiVersion Version { get; set; }
+
+    }
+}

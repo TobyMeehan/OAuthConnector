99d9c35758829d9cc9f613a79571e3f0f3fecf23
Toby <toby@tobymeehan.com>
2020-08-04 22:11:23 +0100

    Add scoreboard class
    Added scoreboard class to wrap objectives

---
+++ TobyMeehan.OAuth/Models/IScoreboard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TobyMeehan.OAuth.Collections;
+
+namespace TobyMeehan.OAuth.Models
+{
+    public interface IScoreboard
+    {
+        IEntityCollection<IObjective> Objectives { get; }
+    }
+}
---
+++ TobyMeehan.OAuth/Models/Scoreboard.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TobyMeehan.OAuth.Collections;
+using TobyMeehan.OAuth.Controllers;
+
+namespace TobyMeehan.OAuth.Models
+{
+    public class Scoreboard : IScoreboard
+    {
+        private readonly IScoreboardController _controller;
+
+        public Scoreboard(IScoreboardController controller)
+        {
+            _controller = controller;
+        }
+
+        public static async Task<Scoreboard> CreateAsync(IScoreboardController controller, CancellationToken cancellationToken)
+        {
+            return new Scoreboard(controller)
+            {
+                Objectives = await controller.GetAsync(cancellationToken)
+            };
+        }
+
+        public IEntityCollection<IObjective> Objectives { get; set; }
+    }
+}
